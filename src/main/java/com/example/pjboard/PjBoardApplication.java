575d57855d8fc8042589e84966e2a885c37b3e02
koreaunnie <hyunjung1995@gmail.com>
2024-10-28 12:21:25 +0900

    first commit

---
+++ src/main/java/com/example/pjboard/PjBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.pjboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjBoardApplication.class, args);
+    }
+
+}
